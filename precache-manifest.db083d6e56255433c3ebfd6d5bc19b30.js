be50bd3016b6d3103d837465bc9c0e442442e694
Micah Manning <micah.manning@newwave.io>
2020-03-31 10:14:59 -0400

    Updates

---
+++ precache-manifest.db083d6e56255433c3ebfd6d5bc19b30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682497fdc897b257799e7a2f936197b2",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "787c84f6d04561b13622",
+    "url": "/monsters-rolodex-complete/static/css/main.539b450d.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/monsters-rolodex-complete/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "787c84f6d04561b13622",
+    "url": "/monsters-rolodex-complete/static/js/main.57f2969e.chunk.js"
+  },
+  {
+    "revision": "683efa6afd61321d8334",
+    "url": "/monsters-rolodex-complete/static/js/runtime~main.0e367587.js"
+  }
+]);
